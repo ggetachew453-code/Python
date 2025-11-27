8f5c088e1c662ed7a60e5a838ecfe6b30714da62
ggetachew453-code <ggetachew453@gmail.com>
2025-11-27 19:48:24 +0300

    Update combination generation.py

--- combination generation.py
+++ combination generation.py
@@ -1,12 +1,12 @@
 def generate_combinations(sequence, k):
    
     def _combine(start, current_comb):
-        # Base case: combination of desired length found
+        
         if len(current_comb) == k:
             yield tuple(current_comb)
             return
         
-        # Recursive case: build combination
+        
         for i in range(start, len(sequence)):
             current_comb.append(sequence[i])
             yield from _combine(i + 1, current_comb)
@@ -17,4 +17,4 @@ def generate_combinations(sequence, k):
 print("Combinations:")
 for comb in generate_combinations([1, 2, 3], 2):
     print(comb, end=" ")
-print()  # Output: (1, 2) (1, 3) (2, 3)
+print()  
