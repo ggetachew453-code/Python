78369be3496b4879b650194bbe7b15ea6e6ff467
ggetachew453-code <ggetachew453@gmail.com>
2025-11-27 19:39:22 +0300

    Update chainsequence.py

--- chainsequence.py
+++ chainsequence.py
@@ -1,11 +1,5 @@
 class chain_sequences:
-    """
-    Iterator that chains multiple sequences together, yielding elements
-    from each sequence in order.
-    
-    Args:
-        *sequences: Variable number of sequences to chain together
-    """
+   
     def __init__(self, *sequences):
         self.sequences = sequences
         self.seq_index = 0
@@ -23,14 +17,14 @@ class chain_sequences:
                 self.element_index += 1
                 return element
             else:
-                # Move to next sequence
+                
                 self.seq_index += 1
                 self.element_index = 0
         
         raise StopIteration
 
-# Example usage
+
 print("Chain Sequences:")
 for item in chain_sequences([1, 2, 3], [4], [5]):
     print(item, end=" ")
-print()  # Output: 1 2 3 4 5
+print() 
